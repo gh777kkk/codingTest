8a4c6c994f2e83dd7a943e9da30bac8c14f085bd
gh <wkdrjswkd5@naver.com>
2022-04-15 23:27:43 +0900

    -

---
+++ src/baekjoon/ct0413/quiz2.java
@@ -0,0 +1,64 @@
+package baekjoon.ct0413;
+
+
+import java.io.*;
+import java.util.*;
+
+/*
+문제
+숫자 카드는 정수 하나가 적혀져 있는 카드이다. 상근이는 숫자 카드 N개를 가지고 있다. 정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자 카드를 상근이가 가지고 있는지 아닌지를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 상근이가 가지고 있는 숫자 카드의 개수 N(1 ≤ N ≤ 500,000)이 주어진다. 둘째 줄에는 숫자 카드에 적혀있는 정수가 주어진다. 숫자 카드에 적혀있는 수는 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다. 두 숫자 카드에 같은 수가 적혀있는 경우는 없다.
+
+셋째 줄에는 M(1 ≤ M ≤ 500,000)이 주어진다. 넷째 줄에는 상근이가 가지고 있는 숫자 카드인지 아닌지를 구해야 할 M개의 정수가 주어지며, 이 수는 공백으로 구분되어져 있다. 이 수도 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다
+
+출력
+첫째 줄에 입력으로 주어진 M개의 수에 대해서, 각 수가 적힌 숫자 카드를 상근이가 가지고 있으면 1을, 아니면 0을 공백으로 구분해 출력한다.
+ */
+public class quiz2 {
+    public static void main(String[] arg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] list_n = new int[n];
+        for (int i = 0; i < n; i++){
+            list_n[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(list_n);
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        br.close();
+        for (int i = 0; i < m; i++){
+            int data = Integer.parseInt(st.nextToken());
+            sb.append(binarySearch(list_n, n, data) + " ");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    public static int binarySearch(int[] cards, int N, int search) {
+        int first = 0;
+        int last = N - 1;
+        int mid = 0;
+
+        while (first <= last) {
+            mid = (first + last) / 2; // 중간 인덱스
+
+            if (cards[mid] == search) { // 중간값과 찾으려는 수가 같은 경우
+                return 1;
+            }
+
+            if (cards[mid] < search) { // 중간값이 찾으려는 수보다 작으면, 그 이하로는 볼 필요 없음.
+                first = mid + 1;
+            } else { // 중간값이 찾으려는 수보다 크면, 그 이상으로는 볼 필요 없음.
+                last = mid - 1;
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ src/baekjoon/ct0415/quiz4.java
@@ -0,0 +1,63 @@
+package baekjoon.ct0415;
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+문제
+정사각형으로 이루어져 있는 섬과 바다 지도가 주어진다. 섬의 개수를 세는 프로그램을 작성하시오.
+
+
+
+한 정사각형과 가로, 세로 또는 대각선으로 연결되어 있는 사각형은 걸어갈 수 있는 사각형이다.
+
+두 정사각형이 같은 섬에 있으려면, 한 정사각형에서 다른 정사각형으로 걸어서 갈 수 있는 경로가 있어야 한다. 지도는 바다로 둘러싸여 있으며, 지도 밖으로 나갈 수 없다.
+
+입력
+입력은 여러 개의 테스트 케이스로 이루어져 있다. 각 테스트 케이스의 첫째 줄에는 지도의 너비 w와 높이 h가 주어진다. w와 h는 50보다 작거나 같은 양의 정수이다.
+
+둘째 줄부터 h개 줄에는 지도가 주어진다. 1은 땅, 0은 바다이다.
+
+입력의 마지막 줄에는 0이 두 개 주어진다.
+
+출력
+각 테스트 케이스에 대해서, 섬의 개수를 출력한다.
+ */
+public class quiz4 {
+    private static int result = -1;
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] list = new int[m][2];
+        for (int i = 0; i < m; i++){
+            list[i][0] = sc.nextInt();
+            list[i][1] = sc.nextInt();
+        }
+        dfs (list,x,y,m,0);
+        System.out.println(result);
+    }
+    private static void dfs(int[][] list, int x, int y, int m, int cnt){
+        if (x == y) {
+            result = cnt;
+            return;
+        }
+        for (int i = 0; i < m; i++){
+            if(list[i][0] == x && list[i][0] != 0){
+                x = list[i][1];
+                list[i][0] = 0;
+                list[i][1] = 0;
+                dfs(list,x,y,m,++cnt);
+            }
+            if(list[i][1] == x && list[i][1] != 0){
+                x = list[i][0];
+                list[i][0] = 0;
+                list[i][1] = 0;
+                dfs(list,x,y,m,++cnt);
+            }
+        }
+    }
+}
